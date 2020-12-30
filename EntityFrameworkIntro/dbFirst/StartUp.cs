c2b85309b5300440dd5db8edd69704bff5cfe463
elichka <ed44362072@edu.mon.bg>
2020-12-30 21:44:03 +0200

    EFC Introduction Initial Commit.

---
+++ EntityFrameworkIntro/dbFirst/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using dbFirst.Data;
+
+namespace dbFirst
+{
+    class Program
+    {
+        //onlyTheLastProblem
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            var town = db.Towns
+                .FirstOrDefault(x => x.Name == "Seattle");
+
+            var addressesInSeattle = db.Addresses
+                .Where(x => x.Town == town)
+                .ToList();
+
+            int count = addressesInSeattle.Count;
+            foreach (var address in addressesInSeattle)
+            {
+                var employees = db.Employees.Where(x => x.Address == address).ToList();
+                foreach (var employee in employees)
+                {
+                    employee.Address = null;
+                    employee.AddressId = null;
+                }
+                db.Addresses.Remove(address);
+            }
+
+            db.Towns.Remove(town!);
+            Console.WriteLine($"{count} addresses in Seattle were deleted");
+            db.SaveChanges();
+        }
+    }
+}
