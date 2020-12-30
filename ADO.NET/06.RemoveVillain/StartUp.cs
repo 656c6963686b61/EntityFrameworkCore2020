92206745f688f87dc47be008de30a6f9af43fca9
elichka <ed44362072@edu.mon.bg>
2020-12-30 18:41:49 +0200

    Solved Problem 6.

--- ADO.NET/06.RemoveVillain/StartUp.cs
+++ ADO.NET/06.RemoveVillain/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 
 namespace _06.RemoveVillain
@@ -7,46 +8,126 @@ namespace _06.RemoveVillain
     {
         static void Main(string[] args)
         {
-            int villainIdToDelete = int.Parse(Console.ReadLine()!);
+            int villainId = int.Parse(Console.ReadLine()!);
             string connectionString = "Server=.; Database=MinionsDB; Integrated Security = true;";
             var sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
 
             using (sqlConnection)
             {
-                // Start a local transaction.
                 var sqlTransaction = sqlConnection.BeginTransaction();
 
-                // Enlist a command in the current transaction.
-                var command = sqlConnection.CreateCommand();
-                command.Transaction = sqlTransaction;
+                var getVillainName = new SqlCommand(@$"SELECT Name 
+	                                                    FROM Villains
+	                                                    WHERE Id = @villainId;", sqlConnection);
+                getVillainName.Parameters.AddWithValue("@villainId", villainId);
+                getVillainName.Transaction = sqlTransaction;
+                var name = ExecuteScalarCommand(getVillainName, sqlTransaction);
 
-                try
+                if (name == null)
                 {
-                    //execute command
-                    command.ExecuteScalar();
-
-                    sqlTransaction.Commit();
-                    Console.WriteLine("Both records were written to database.");
+                    Console.WriteLine("No such villain was found.");
                 }
-                catch (Exception ex)
+
+
+                var findMinions = new SqlCommand(@$"SELECT M.Id
+	                                                  FROM Minions AS M
+	                                                  JOIN MinionsVillains AS MV ON MV.MinionId = M.Id
+	                                                  JOIN Villains AS V ON V.Id = MV.VillainId
+	                                                  WHERE V.Id = @villainId", sqlConnection);
+                findMinions.Parameters.AddWithValue("@villainId",villainId);
+                findMinions.Transaction = sqlTransaction;
+                
+                var reader = (SqlDataReader)ExecuteReaderCommand(findMinions, sqlTransaction);
+                var minionIds = new List<object>();
+
+                using (reader)
                 {
-                    // Handle the exception if the transaction fails to commit.
-                    Console.WriteLine(ex.Message);
-
-                    try
+                    while (reader.Read())
                     {
-                        // Attempt to roll back the transaction.
-                        sqlTransaction.Rollback();
-                    }
-                    catch (Exception exRollback)
-                    {
-                        // Throws an InvalidOperationException if the connection
-                        // is closed or the transaction has already been rolled
-                        // back on the server.
-                        Console.WriteLine(exRollback.Message);
+                        minionIds.Add(reader[0]);
                     }
                 }
+                
+
+                foreach (var id in minionIds)
+                {
+                    var deleteFromConnectionTable = new SqlCommand(@$"DELETE 
+	                                                                 FROM MinionsVillains
+	                                                                 WHERE MinionId = @minionId", sqlConnection);
+                    deleteFromConnectionTable.Parameters.AddWithValue("@minionId", id);
+                    deleteFromConnectionTable.Transaction = sqlTransaction;
+                    ExecuteNonQueryCommand(deleteFromConnectionTable, sqlTransaction);
+
+
+                    var deleteMinions = new SqlCommand(@$"DELETE 
+	                                                    FROM Minions
+	                                                    WHERE Id = @minionId;", sqlConnection);
+                    deleteMinions.Parameters.AddWithValue("@minionId", id);
+                    deleteMinions.Transaction = sqlTransaction;
+                    ExecuteNonQueryCommand(deleteMinions, sqlTransaction);
+                }
+
+                
+
+                var deleteVillainFromConnectionTable = new SqlCommand(@$"DELETE 
+	                                                                 FROM MinionsVillains
+	                                                                 WHERE VillainId = @villainId", sqlConnection);
+                deleteVillainFromConnectionTable.Parameters.AddWithValue("@villainId", villainId);
+                deleteVillainFromConnectionTable.Transaction = sqlTransaction;
+                ExecuteNonQueryCommand(deleteVillainFromConnectionTable, sqlTransaction);
+
+                var deleteVillain = new SqlCommand(@$"DELETE 
+	                                                    FROM Villains
+	                                                    WHERE Id = @villainId;", sqlConnection);
+                deleteVillain.Parameters.AddWithValue("@villainId", villainId);
+                deleteVillain.Transaction = sqlTransaction;
+                ExecuteNonQueryCommand(deleteVillain, sqlTransaction);
+
+               
+                Console.WriteLine($"{name} was deleted.");
+                Console.WriteLine($"{minionIds.Count} minions were released.");
+            }
+        }
+
+        private static object ExecuteScalarCommand(SqlCommand command, SqlTransaction transaction)
+        {
+            try
+            {
+                return command.ExecuteScalar();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                transaction.Rollback();
+                return null;
+            }
+        }
+
+        private static object ExecuteReaderCommand(SqlCommand command, SqlTransaction transaction)
+        {
+            try
+            {
+                return command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                transaction.Rollback();
+                return null;
+            }
+        }
+
+        private static void ExecuteNonQueryCommand(SqlCommand command, SqlTransaction transaction)
+        {
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                transaction.Rollback();
             }
         }
     }
