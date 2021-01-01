64fed5bc0bb48d299fefb33eb2ed780d333634ff
elichka <ed44362072@edu.mon.bg>
2021-01-01 17:46:22 +0200

    GetBooksByAgeRestriction needs fixing :/

--- Advanced Querying/BookShop/StartUp.cs
+++ Advanced Querying/BookShop/StartUp.cs
@@ -1,14 +1,50 @@
 ﻿namespace BookShop
 {
+    using System;
+    using System.ComponentModel.Design;
+    using System.Linq;
+    using System.Text;
     using Data;
-    using Initializer;
+    using Models.Enums;
 
     public class StartUp
     {
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
+            var ageRestriction = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, ageRestriction));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            //age restriction is stored as a number in the database
+            //to string doesn't work
+
+            AgeRestriction ageRestriction = 0;
+
+            if (command.ToLower() == "minor")
+            {
+                ageRestriction = AgeRestriction.Minor;
+            }
+            else if (command.ToLower() == "teen")
+            {
+                ageRestriction = AgeRestriction.Teen;
+            }
+            else if (command.ToLower() == "adult")
+            {
+                ageRestriction = AgeRestriction.Adult;
+            }
+
+            var result = context
+                .Books
+                .Where(x => x.AgeRestriction == ageRestriction)
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .Take(10)
+                .ToList();
+            return string.Join("\n", result);
         }
     }
 }
